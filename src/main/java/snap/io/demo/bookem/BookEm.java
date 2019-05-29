df84ff207cf50fbcab3a7afcff7f53b1c61b05b1
dbmartin00 <david.martin@split.io>
2019-05-29 07:17:08 -0700

    event properties

--- src/main/java/snap/io/demo/bookem/BookEm.java
+++ src/main/java/snap/io/demo/bookem/BookEm.java
@@ -1,25 +1,25 @@
 package snap.io.demo.bookem;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 import java.util.UUID;
+import java.util.concurrent.TimeoutException;
 
 import io.split.client.SplitClient;
 import io.split.client.SplitClientConfig;
-import io.split.client.SplitFactory;
 import io.split.client.SplitFactoryBuilder;
 
 public class BookEm {
-	
+
 	class BookEmHost {
 		String guid;
 		float satisfaction;
 		int listing_views;
 		float percentage_days_vacant;
-		
+
 		public BookEmHost(String guid, float satisfaction, int listing_views, float percentage_days_vacant) {
 			super();
 			this.guid = guid;
@@ -59,7 +59,7 @@ public class BookEm {
 		public void setPercentage_days_vacant(float percentage_days_vacant) {
 			this.percentage_days_vacant = percentage_days_vacant;
 		}
-		
+
 	}
 
 	public static final int TOTAL_HOST_COUNT = 10000;
@@ -69,81 +69,77 @@ public class BookEm {
 	Random random = new Random(System.currentTimeMillis());
 	private void runSeason() throws Exception {
 		long total_start = System.currentTimeMillis();
+		
+		// Construct one season of BookEm hosts
 		System.out.println("Start construction of " + TOTAL_HOST_COUNT + " unique hosts");
 		for(int i = 0; i < TOTAL_HOST_COUNT; i++) {
 
 			float satisfaction = 40 + random.nextInt(30);
 			int listing_views = random.nextInt(25);
 			float percentage_days_vacant = (listing_views > 0 ? (random.nextInt(20) + 40) : 100);
-			
+
 			BookEmHost host = new BookEmHost("" + UUID.randomUUID(), satisfaction, listing_views, percentage_days_vacant);
 			allHosts.add(host);
 		}
-		
-		// Add the critics
-		allHosts.add(new BookEmHost("aaa-guide", 4 + random.nextInt(3), 0, 100));
-		allHosts.add(new BookEmHost("fodors-guide", 4 + random.nextInt(3), 0, 100));
-		allHosts.add(new BookEmHost("michelin-guide", 4 + random.nextInt(3), 0, 100));
-		allHosts.add(new BookEmHost("lonelyplanet-guide", 4 + random.nextInt(3), 0, 100));
-		allHosts.add(new BookEmHost("bourdain-guide", 4 + random.nextInt(3), 0, 100));
-		
-		SplitClientConfig config = SplitClientConfig.builder().ready(60000).eventFlushIntervalInMillis(1000).build();
-		SplitFactory splitFactory = SplitFactoryBuilder.build("67u****PROVIDE SDK KEY FROM SPLIT ADMIN UI***", config);
-		SplitClient client = splitFactory.client();
-		
+
+		// Connect Split Client
+		SplitClientConfig config = SplitClientConfig.builder()
+				.setBlockUntilReadyTimeout(60000)
+				.build();
+
+		SplitClient client = SplitFactoryBuilder.build("*** FIXME PROVIDE SDK KEY ***", config).client();
 		try {
+			client.blockUntilReady();
+		} catch (TimeoutException | InterruptedException e) {
+			throw e;
+		}
+
+		try {
+			Map<String, Object> properties_us = new TreeMap<String, Object>();
+			properties_us.put("region", "us");
+			Map<String, Object> properties_eu = new TreeMap<String, Object>();
+			properties_eu.put("region", "eu");
+			Map<String, Object> properties_jp = new TreeMap<String, Object>();
+			properties_jp.put("region", "jp");
+			
 			int count = 1;
+			// Check if each host gets "instant booking" feature
 			for(BookEmHost host : allHosts) {
-				boolean hasGottenTreatment = false;
-				
-				if(client.getTreatment("" + host.getGuid(), "instant_booking").equalsIgnoreCase("on")) {
+
+				// generates an Impression with no further action
+				String treatment = client.getTreatment("" + host.getGuid(), "instant_booking");
+				if(treatment.equalsIgnoreCase("on")) {
 					host.setListing_views(host.getListing_views() + random.nextInt(25));
 					host.setPercentage_days_vacant(host.getPercentage_days_vacant() - random.nextInt(10));
 					host.setSatisfaction(host.getSatisfaction() - random.nextInt(30));
 					System.out.print("a");
-					hasGottenTreatment = true;
-				}
-				
-				Map<String, Object> attributes = new HashMap<String, Object>();
-				String region = random.nextInt(100) < 80 ? "EU" : "US";
-				attributes.put("region", region);
-				if(client.getTreatment(host.getGuid(), "search_engine_optimization", attributes).equalsIgnoreCase("on")) {
-					host.setListing_views(host.getListing_views() + 40 + random.nextInt(20));
-					host.setPercentage_days_vacant(host.getPercentage_days_vacant() - random.nextInt(3));
+				} else if (treatment.equalsIgnoreCase("off")){
 					System.out.print("b");
-					hasGottenTreatment = true;
+				} else {
+					System.out.println("x");
 				}
 				
-				if(client.getTreatment("" + host.getGuid(), "background_check").equalsIgnoreCase("on")) {
-					host.setSatisfaction(host.getSatisfaction() + random.nextInt(15));
-					System.out.print("c");
-					hasGottenTreatment = true;
-				}
+				// CRITICAL send the event data to Split with an SDK track call
+
 				
-				if(!hasGottenTreatment) {
-					System.out.print("+");
+				Map<String, Object> properties = new TreeMap<String, Object>();
+				int region = random.nextInt(3);
+				if(region == 0) {
+					properties = properties_us;
+				} else if (region == 1) {
+					properties = properties_eu;
+				} else if (region == 2) {
+					properties = properties_jp;
 				}
+				client.track(host.getGuid(), "user", "host_satisfaction", host.getSatisfaction(), properties);
+				client.track(host.getGuid(), "user", "listing_views", host.getListing_views(), properties);
+				client.track(host.getGuid(), "user", "percentage_days_vacant_per_month", host.getPercentage_days_vacant(), properties);
+
+//				client.track(host.getGuid(), "user", "host_satisfaction", host.getSatisfaction());
+//				client.track(host.getGuid(), "user", "listing_views", host.getListing_views());
+//				client.track(host.getGuid(), "user", "percentage_days_vacant_per_month", host.getPercentage_days_vacant());
+		
 				
-				client.track(host.getGuid(), "host", "host_satisfaction", host.getSatisfaction());
-				client.track(host.getGuid(), "host", "listing_views", host.getListing_views());
-				client.track(host.getGuid(), "host", "percentage_days_vacant_per_month", host.getPercentage_days_vacant());
-				
-				String treatment = client.getTreatment(host.getGuid(), "new_service");
-				System.out.print(treatment);
-				if(treatment.equalsIgnoreCase("housekeeping")) {
-					client.track(host.getGuid(), "host", "bem_housekeeping", random.nextInt(10));					
-				} else if (treatment.equalsIgnoreCase("accounting")) {
-					client.track(host.getGuid(), "host", "bem_accounting", random.nextInt(2));					
-					
-				} else if (treatment.equalsIgnoreCase("gardening")) {
-					client.track(host.getGuid(), "host", "bem_gardening", random.nextInt(5));					
-					
-				} else if (treatment.equalsIgnoreCase("laundry")) {
-					client.track(host.getGuid(), "host", "bem_laundry", random.nextInt(20));					
-					
-				} else if (treatment.equalsIgnoreCase("food_delivery")) {
-					client.track(host.getGuid(), "host", "bem_food_delivery", random.nextInt(5));					
-				}
 				
 				if(count++ % 100 == 0) {
 					System.out.println();
@@ -153,12 +149,12 @@ public class BookEm {
 			}
 
 		} finally {
-			splitFactory.destroy();
+			client.destroy();
 		}
 
 		System.out.println("finished a season in " + (System.currentTimeMillis() - total_start) + "ms");
 	}
-	
+
 	public void execute() throws Exception {
 		while(true) {
 			runSeason();
@@ -166,6 +162,11 @@ public class BookEm {
 	}
 
 
+	// Add the critics
+//	allHosts.add(new BookEmHost("aaa-guide", 4 + random.nextInt(3), 0, 100));
+//	allHosts.add(new BookEmHost("fodors-guide", 4 + random.nextInt(3), 0, 100));
+//	allHosts.add(new BookEmHost("michelin-guide", 4 + random.nextInt(3), 0, 100));
+//	allHosts.add(new BookEmHost("lonelyplanet-guide", 4 + random.nextInt(3), 0, 100));
+//	allHosts.add(new BookEmHost("bourdain-guide", 4 + random.nextInt(3), 0, 100));
 
-	
 }
